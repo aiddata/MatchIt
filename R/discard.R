cce643aa550446d80a7291540e19eab77d95e8bd
userz <sgoodman@aiddata.org>
2016-03-18 15:20:33 -0400

    Cleanup.

--- R/discard.R
+++ R/discard.R
@@ -6,15 +6,20 @@ discard <- function(treat, pscore, option, X) {
   pmin0 <- min(pscore[treat == 0])
   pmin1 <- min(pscore[treat == 1])
 
-  if (is.logical(option)) {       # user input
+  if (is.logical(option)) {
+    # user input
     return(option)
-  } else if (option == "none") {   # keep all units
+  } else if (option == "none") {
+    # keep all units
     discarded <- rep(FALSE, n.obs)
-  } else if (option == "both") {   # discard units outside of common support
+  } else if (option == "both") {
+    # discard units outside of common support
     discarded <- (pscore < max(pmin0, pmin1) | pscore > min(pmax0, pmax1))
-  } else if (option == "control") { # discard control units only
+  } else if (option == "control") {
+    # discard control units only
     discarded <- (pscore < pmin1 | pscore > pmax1)
-  } else if (option == "treat") {  # discard treated units only
+  } else if (option == "treat") {
+    # discard treated units only
     discarded <- (pscore < pmin0 | pscore > pmax0)
   } else if (any(grep(option, c("hull.control", "hull.treat", "hull.both")))) {
 
@@ -30,13 +35,15 @@ discard <- function(treat, pscore, option, X) {
     # require(lpSolve)
     discarded <- rep(FALSE, n.obs)
 
-    if (option == "hull.control") { # discard units not in T convex hull
+    if (option == "hull.control") {
+      # discard units not in T convex hull
       wif <- whatif(cfact=X[treat == 0,], data=X[treat == 1,])
       discarded[treat == 0] <- !wif$in.hull
     } else if (option == "hull.treat") {
       wif <- whatif(cfact=X[treat == 1,], data=X[treat == 0,])
       discarded[treat == 1] <- !wif$in.hull
-    } else if (option == "hull.both"){ # discard units not in T&C convex hull
+    } else if (option == "hull.both") {
+      # discard units not in T&C convex hull
       wif <- whatif(cfact=cbind(1-treat, X), data=cbind(treat, X))
       discarded <- !wif$in.hull
     } else {
--- R/distance2mahalanobis.R
+++ R/distance2mahalanobis.R
@@ -1,8 +1,8 @@
 distance2mahalanobis <- function(formula, data, ...) {
   X <- model.matrix(formula, data)
-  ## Placeholder where real work is done on a unit by unit basis
+
+  # Placeholder where real work is done on a unit by unit basis
   distance <- rep(1, nrow(X))
-  return(list(model = NULL,
-              distance = distance))
+  return(list(model = NULL, distance = distance))
 }
 
