83653ab21234b02728d19914e0f465a066c322cd
userz <sgoodman@aiddata.org>
2016-04-01 12:10:20 -0400

    Cleanup misc.

--- R/matchit2optimal.R
+++ R/matchit2optimal.R
@@ -20,21 +20,22 @@ matchit2optimal <- function(treat, X, data, distance, discarded,
   tlabels <- rownames(d) <- names(ttt[ttt == 1])
   clabels <- colnames(d) <- names(ttt[ttt == 0])
   for (i in 1:n1) {
-    d[i,] <- abs(d1[i]-d0)
+    d[i, ] <- abs(d1[i] - d0)
   }
 
   full <- fullmatch(d, min.controls = ratio,
                     max.controls = ratio,
                     omit.fraction = (n0-ratio*n1)/n0, ...)
+
   psclass <- full[pmatch(names(ttt), names(full))]
   psclass <- as.numeric(as.factor(psclass))
   names(psclass) <- names(ttt)
 
-  mm <- matrix(0, nrow = n1, ncol = ratio,
-               dimnames = list(tlabels, 1:ratio))
+  mm <- matrix(0, nrow=n1, ncol=ratio,
+               dimnames=list(tlabels, 1:ratio))
 
   for (i in 1:n1) {
-    mm[i,] <- names(which(
+    mm[i, ] <- names(which(
                 psclass[tlabels[i]] == psclass[-pmatch(tlabels[i],
                                                        names(psclass))]))
   }
@@ -48,10 +49,10 @@ matchit2optimal <- function(treat, X, data, distance, discarded,
     ## add match.matrix = NA for discarded units
     tdisc <- discarded[treat == 1]
     if (any(tdisc)) {
-      tmp <- matrix(NA, nrow = sum(tdisc), ncol= ratio,
-                    dimnames = list(names(treat[treat == 1 & discarded]),
+      tmp <- matrix(NA, nrow=sum(tdisc), ncol=ratio,
+                    dimnames=list(names(treat[treat == 1 & discarded]),
                     1:ratio))
-      mm <- as.matrix(rbind(mm, tmp)[names(treat[treat == 1]),])
+      mm <- as.matrix(rbind(mm, tmp)[names(treat[treat == 1]), ])
     }
   }
 
